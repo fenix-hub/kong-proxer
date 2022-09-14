d85a2fdc41a5420742a05a616250cfc2d71324d7
Nicolò Santilio <nicolo.santilio@outlook.com>
2022-09-14 16:52:04 +0200

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -4,7 +4,7 @@ return {
         proxer_host = {required = true, type = 'string'},
         proxer_port = {required = true, type = 'number', default = 80},
         proxer_ssl_port = {required = false, type = 'number', default = 443},
-        proxer_schema_header = {required = true, type = 'string', default = 'X-proxerTo-Schema'},
-        proxer_host_header = {required = true, type = 'string', default = 'X-proxerTo-Host'}
+        proxer_schema_header = {required = true, type = 'string', default = 'X-ProxyTo-Schema'},
+        proxer_host_header = {required = true, type = 'string', default = 'X-ProxyTo-Host'}
     }
 }
