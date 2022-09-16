4647652a252aba12403f07bb4af6d26b7319f399
Nicolò Santilio <nicolo.santilio@outlook.com>
2022-09-16 11:27:37 +0200

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -1,11 +1,11 @@
 return {
     name = "kong-proxer",
-    fields = {
+    fields = [
         proxer_schema = {required = false, type = 'string'},
         proxer_host = {required = true, type = 'string'},
         proxer_port = {required = true, type = 'number', default = 80},
         proxer_ssl_port = {required = false, type = 'number', default = 443},
         proxer_schema_header = {required = true, type = 'string', default = 'X-ProxyTo-Schema'},
         proxer_host_header = {required = true, type = 'string', default = 'X-ProxyTo-Host'}
-    }
+    ]
 }
