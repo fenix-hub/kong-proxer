7c9333c3e3339600e2ab287f32eb5cccb4ca7016
Nicolò Santilio <nicolo.santilio@outlook.com>
2022-09-16 11:48:02 +0200

    Update handler.lua

--- src/handler.lua
+++ src/handler.lua
@@ -4,27 +4,33 @@ local ProxerHandler = {
 }
 
 function ProxerHandler.access(self, config)
-    local target_host = ngx.ctx.balancer_address.host .. ":" .. ngx.ctx.balancer_address.port
-    kong.log("Forwarding request for: " .. target_host)
-    
-    local proxer_schema, proxer_host, proxer_port, proxer_ssl_port = config.proxer_schema, config.proxer_host, config.proxer_port, config.proxer_ssl_port
-    
-    if proxer_schema == '' or proxer_schema == nil then
-        proxer_schema = ngx.ctx.balancer_address.schema
-    end
-    if proxer_schema == "https" then
-        proxer_port = proxer_ssl_port
-    end
-    
-    ngx.ctx.balancer_address.schema = proxer_schema
-    ngx.ctx.balancer_address.host = proxer_host
-    ngx.ctx.balancer_address.port = proxer_port
-   
-    ngx.req.set_header(config.proxer_schema_header, target_schema)
-    ngx.req.set_header(config.proxer_host_header, target_host)
-    
-    
-    kong.log("Forwarding request to: " .. proxer_schema .. "://" .. proxer_host .. ":" .. proxer_port)
+  for index, value in ipairs(ngx.ctx.service.tags) do
+      if value == "hosted" then
+          return
+      end
+  end
+  
+  local target_host = ngx.ctx.balancer_address.host .. ":" .. ngx.ctx.balancer_address.port
+  kong.log("Forwarding request for: " .. target_host)
+
+  local proxer_schema, proxer_host, proxer_port, proxer_ssl_port = config.proxer_schema, config.proxer_host, config.proxer_port, config.proxer_ssl_port
+
+  if proxer_schema == '' or proxer_schema == nil then
+      proxer_schema = ngx.ctx.balancer_address.schema
+  end
+  if proxer_schema == "https" then
+      proxer_port = proxer_ssl_port
+  end
+
+  ngx.ctx.balancer_address.schema = proxer_schema
+  ngx.ctx.balancer_address.host = proxer_host
+  ngx.ctx.balancer_address.port = proxer_port
+
+  ngx.req.set_header(config.proxer_schema_header, target_schema)
+  ngx.req.set_header(config.proxer_host_header, target_host)
+
+
+  kong.log("Forwarding request to: " .. proxer_schema .. "://" .. proxer_host .. ":" .. proxer_port)
 end
 
 return ProxerHandler
