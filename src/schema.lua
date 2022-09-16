f08c09feb58a4dad9ea1fddb09b604aa75c56736
Nicolò Santilio <nicolo.santilio@outlook.com>
2022-09-16 11:43:02 +0200

    Update schema.lua

--- src/schema.lua
+++ src/schema.lua
@@ -12,8 +12,8 @@ return {
                     { proxer_host = {required = true, type = "string"} },
                     { proxer_port = {required = true, type = "number", default = 80} },
                     { proxer_ssl_port = {required = false, type = "number", default = 443} },
-                    { proxer_schema_header = {required = true, type = "number", default = "X-ProxyTo-Schema"} },
-                    { proxer_host_header = {required = true, type = "number", default = "X-ProxyTo-Host"} }   
+                    { proxer_schema_header = {required = true, type = "string", default = "X-ProxyTo-Schema"} },
+                    { proxer_host_header = {required = true, type = "string", default = "X-ProxyTo-Host"} }   
                 }
             }
         }
